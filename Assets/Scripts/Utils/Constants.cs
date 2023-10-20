e539a86a4a2c32aab91bcf0b6305910fe210be2a
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-10-20 17:19:32 +0200

    created Countdown constants

---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils
+{
+    public static class PieceCountdowns
+    {
+        public const int Pawn = 1;
+        public const int Rook = 2;
+        public const int Knight = 2;
+        public const int Bishop = 2;
+        public const int Queen = 1;
+        public const int King = 1;
+    }
+}
