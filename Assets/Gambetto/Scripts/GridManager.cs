828491cd91465dc30c5f65a041687442e312a4f1
Mario2414 <mario.vallone@mail.polimi.it>
2023-10-22 18:21:30 +0200

    Added some getter

--- Assets/Gambetto/Scripts/GridManager.cs
+++ Assets/Gambetto/Scripts/GridManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     private GameObject roomPrefab;
+    private int lastColor = 0;
 
 
     void Start()
@@ -18,9 +19,32 @@ public class GridManager : MonoBehaviour
         foreach (RoomLayout room in rooms)
         {
             GameObject roomObj = Instantiate(roomPrefab,  transform.position, Quaternion.identity);
+            roomObj.GetComponent<Room>().setColorStart(lastColor);
             roomObj.GetComponent<Room>().InitializeRoom(room);
             roomObj.transform.position = translation;
-            translation = translation + new Vector3(7,0,0);
+            translation = translation + new Vector3(room.GetSizeRow(),0,0);
+            if (room.GetSizeRow() % 2 == 0 && lastColor == 1)
+            {
+                if (lastColor == 1)
+                {
+                    lastColor = 1;
+                }
+                else
+                {
+                    lastColor = 0;
+                }
+            }
+            else
+            {
+                if (lastColor == 1)
+                {
+                    lastColor = 0;
+                }
+                else
+                {
+                    lastColor = 1;
+                }
+            }
         }
     }
     
