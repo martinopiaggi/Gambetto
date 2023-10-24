a3a54a73f716ea81322cae91a6a594cd1152332c
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-10-24 12:05:39 +0200

    Built graph of a single room with consistent links to neighbors

--- Assets/Gambetto/Scripts/GridManager.cs
+++ Assets/Gambetto/Scripts/GridManager.cs
@@ -5,34 +5,34 @@ using Gambetto.Scripts.Utils;
 
 public class GridManager : MonoBehaviour
 {
-    private GameObject roomPrefab;
-    private int lastColor = 1;
-    private bool changed = false;
+    private GameObject _roomPrefab;
+    private int _lastColor = 1;
+    private bool _changed = false;
 
-    private List<List<Cell>> grid;
+    private List<List<Cell>> _grid;
 
     
     public void CreateGrid(List<RoomLayout> roomLayouts)
     {
-        grid = new List<List<Cell>>();
+        _grid = new List<List<Cell>>();
         Vector3 translation = new Vector3(0,0,0);
         
-        for (int roomIdx = 0; roomIdx <= roomLayouts.Count; roomIdx++)
+        for (var roomIdx = 0; roomIdx < roomLayouts.Count; roomIdx++)
         {
             var roomLayout = roomLayouts[roomIdx];
-            grid.Add(populateRoomGraph(roomLayout,translation,roomIdx));
+            _grid.Add(PopulateRoomGraph(roomLayout,translation,roomIdx));
             
-            GameObject roomObj = Instantiate(roomPrefab,  transform.position, Quaternion.identity);
+            GameObject roomObj = Instantiate(_roomPrefab,  transform.position, Quaternion.identity);
             //if the last color is 0 (white) the starting color will be changed in 1 (blue)
-            if (lastColor == 1)
+            if (_lastColor == 1)
             {
                 roomObj.GetComponent<Room>().setColorStart(0);
-                changed = false;
+                _changed = false;
             }
             else
             {
                 roomObj.GetComponent<Room>().setColorStart(1);
-                changed = true;
+                _changed = true;
             }
             roomObj.GetComponent<Room>().InitializeRoom(roomLayout);
             roomObj.transform.position = translation;
@@ -61,29 +61,29 @@ public class GridManager : MonoBehaviour
             //if the room has an odd lenght and was not chaged its last color is 0 (white)
             if (roomLayout.GetSizeRow() % 2 == 0)
             {
-                if (changed)
+                if (_changed)
                 {
-                    lastColor = 0;
+                    _lastColor = 0;
                 }
                 else
                 {
-                    lastColor = 1;
+                    _lastColor = 1;
                 }
                 
             }
             else
             {
-                if (changed)
+                if (_changed)
                 {
-                    lastColor = 1;
+                    _lastColor = 1;
                 }
                 else
                 {
-                    lastColor = 0;
+                    _lastColor = 0;
                 }
             }
 
-            if (roomIdx > 0) //first room hasn't any previous room to check consinstency of common cells with
+            if (roomIdx > 0) //first room hasn't any previous room to check consistency of common cells with
             {
                 //make consistency of the cells of this room with the previous one
                 //we use the roomLayout exitSide information to compute it 
@@ -93,47 +93,73 @@ public class GridManager : MonoBehaviour
         }
     }
     
-    private List<Cell> populateRoomGraph(RoomLayout roomLayout, Vector3 coordinateOrigin, int roomId)
+    
+    
+
+    private List<Cell> PopulateRoomGraph(RoomLayout roomLayout, Vector3 coordinateOrigin, int roomId)
     {
         //building first a temporary matrix to build easily the graph of cells
-        int[,] squaresInfo = new int[roomLayout.GetSizeRow(), roomLayout.GetSizeColumn()];
-        int rowNumber = 0;
-        foreach (var column in roomLayout.GetRows())
+        var matrixCells = new Cell[roomLayout.GetSizeRow(), roomLayout.GetSizeColumn()];
+        var roomCells = new List<Cell>();
+        for(var rowNumber = 0; rowNumber < roomLayout.GetSizeRow();rowNumber++)
         {
-            int columnNumber = 0;
-            foreach (var square in column.GetColumns())
+            for (var columnNumber = 0; columnNumber < roomLayout.GetSizeColumn(); columnNumber++)
             {
-                squaresInfo[rowNumber, columnNumber] = square;
-                columnNumber++;
-            }
-            rowNumber++;
-        }
-
-        
-        //creating list of cell of a single room 
-        // empty cells are null 
-        List<Cell> roomCells = new List<Cell>();
-        
-        for (int i = 0; i < roomLayout.GetSizeRow(); i++)
-        {
-            for (int j = 0; j < roomLayout.GetSizeColumn(); j++)
-            {
-                if (squaresInfo[i,j] != -1) //if empty space no need to make a Cell object
+                var square = roomLayout.GetRows()[rowNumber].GetColumns()[columnNumber];
+                if (square == -1) continue; //if empty space no need to make a Cell object
+                
+                var cell = new Cell(coordinateOrigin + new Vector3(rowNumber, 0, columnNumber),roomId);
+                roomCells.Add(cell);
+                matrixCells[rowNumber, columnNumber] = cell;
+                //set all the neighbors links updating also neighbors links
+                if (columnNumber >= 1)
                 {
-                    Cell cell = new Cell(coordinateOrigin + new Vector3(i, 0, j),roomId);
-                    //@todo here we have to set all the neighbors links
-                    roomCells.Add(cell);
+                    var nextEast = matrixCells[rowNumber, columnNumber - 1];
+                    if (nextEast != null)
+                    {
+                        cell.setNext(Directions.East,nextEast);
+                        nextEast.setNext(Directions.West,cell); 
+                    }
+                        
+                    if (rowNumber >= 1)
+                    {
+                        var nextSouthEast = matrixCells[rowNumber -1, columnNumber - 1];
+                        if (nextSouthEast != null)
+                        {
+                            cell.setNext(Directions.SouthEast,nextSouthEast);
+                            nextSouthEast.setNext(Directions.NorthWest,cell); 
+                        }
+
+                        if (columnNumber < roomLayout.GetSizeColumn() - 1)
+                        {
+                            var nextSouthWest = matrixCells[rowNumber -1, columnNumber + 1];
+                            if (nextSouthWest != null)
+                            {
+                                cell.setNext(Directions.SouthWest,nextSouthWest);
+                                nextSouthWest.setNext(Directions.NorthEast,cell); 
+                            }
+                        }
+                    }
                 }
+                    
+                if (rowNumber >= 1)
+                {
+                    var nextSouth = matrixCells[rowNumber-1, columnNumber];
+                    if (nextSouth != null)
+                    {
+                        cell.setNext(Directions.South,nextSouth);
+                        nextSouth.setNext(Directions.North,cell); 
+                    }
+                }
+   
+                roomCells.Add(cell);
             }
         }
-
-        Debug.Log(roomCells.Count);
-        
         return roomCells;
     }
     
     private void Awake()
     {
-        roomPrefab = Resources.Load<GameObject>("Prefabs/Room");
+        _roomPrefab = Resources.Load<GameObject>("Prefabs/Room");
     }
 }
