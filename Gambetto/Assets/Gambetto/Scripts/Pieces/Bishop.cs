dc33dc149b55ddc784f6c0d64db28b8ac64e3cbc
zibas <milo.brontesi@gmail.com>
2023-11-11 11:02:44 +0100

    Adds alternate chessBorad Cube prefab

--- Gambetto/Assets/Gambetto/Scripts/Pieces/Bishop.cs
+++ Gambetto/Assets/Gambetto/Scripts/Pieces/Bishop.cs
@@ -6,7 +6,7 @@ namespace Pieces
     public class Bishop : Piece
     {
         ///<summary>
-        ///   <para> Calls parent <see cref="Piece.Awake">Awake</see>, sets the <see cref="PieceType">Piece Type</see>, <see cref="Piece.Countdown">Countdown</see> and the possible moves for the piece.</para>
+        ///  <para> Calls parent <see cref="Piece.Awake">Awake</see>, sets the <see cref="PieceType">Piece Type</see>, <see cref="Piece.Countdown">Countdown</see> and the possible moves for the piece.</para>
         ///  <para> Also sets the mesh for the piece.</para>
         /// </summary>
         private protected void Awake()
