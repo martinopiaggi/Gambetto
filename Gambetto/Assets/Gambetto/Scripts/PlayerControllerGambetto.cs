f77201e77df3e112b7f4498bc19aa4bbc8d5cd1e
Mario2414 <mario.vallone@mail.polimi.it>
2023-11-12 19:00:47 +0100

    Added logic in playerController
    PlayerController class create a light in start, ha un array che può essere riempito con i movimenti dei pezzi, quando è riepmpito illumina le caselle all'interno dell'array ed è in grado di prendere l'input relativo alla casella

--- Gambetto/Assets/Gambetto/Scripts/PlayerControllerGambetto.cs
+++ Gambetto/Assets/Gambetto/Scripts/PlayerControllerGambetto.cs
@@ -1,22 +1,83 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using Pieces;
 using UnityEngine;
 
 public class PlayerControllerGambetto : MonoBehaviour
 {
+    
+    private Light playerLight;
+    private List<Vector3> possibleMovements;
+    private Vector3 currentMove;
+    
     // Start is called before the first frame update
     void Start()
     {
+        GameObject lightObject = new GameObject("PlayerLight");
+        playerLight = lightObject.AddComponent<Light>();
+
+        // Set light properties (you can customize these)
+        playerLight.type = LightType.Point;
+        playerLight.range = 5.0f;
+        playerLight.intensity = 1.0f;
+
+        // Turn off the light initially
+        playerLight.enabled = false;
         
+        currentMove = new Vector3();
+        possibleMovements = new List<Vector3>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (possibleMovements.Count > 0)
+        {
+            // Get the first movement from the list
+            Vector3 nextMovement = possibleMovements[0];
+            // Turn on the light
+            playerLight.enabled = true;
+            // Move the light to the new coordinates
+            transform.Translate(nextMovement);
+        }
+        else
+        {
+            // If the list is empty, turn off the light
+            playerLight.enabled = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (possibleMovements.Count > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("hai premuto spazio");
+                Debug.Log(currentMove);
+                currentMove = possibleMovements[0];
+                possibleMovements.Clear();
+            }
+            else
+            {
+                // Remove the used movement from the list
+                possibleMovements.RemoveAt(0);
+            }
+            
+        }
+    }
+
+    public Vector3 startChoosing(Piece piece, Cell currentPosition)
+    {
+        possibleMovements.Clear();
+        possibleMovements = GetPossibleMovements(piece, currentPosition);
+        StartCoroutine(WaitForFrames(possibleMovements.Count));
+        return currentMove;
     }
     
-    public List<Vector3> GetPossibleMovements(Piece player, Cell currentPosition)
+    private List<Vector3> GetPossibleMovements(Piece player, Cell currentPosition)
         {
             List<Vector3> possibleMovement = new List<Vector3>();
             Cell startingCell = currentPosition;
@@ -117,4 +178,16 @@ public class PlayerControllerGambetto : MonoBehaviour
 
             return possibleMovement;
         }
+    
+    IEnumerator WaitForFrames(int framesToWait)
+    {
+        // Aspetta il numero desiderato di frame
+        for (int i = 0; i < framesToWait; i++)
+        {
+            yield return null; // Aspetta un frame
+        }
+        
+    }
+    
+    
 }
