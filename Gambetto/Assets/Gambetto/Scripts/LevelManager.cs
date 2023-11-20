f4191f19269baf4a8c850bfb9166ff2af9376f12
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-11-20 11:44:16 +0100

    LevelManager is now a singleton

--- Gambetto/Assets/Gambetto/Scripts/LevelManager.cs
+++ Gambetto/Assets/Gambetto/Scripts/LevelManager.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using Gambetto.Scripts;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+    
     public List<RoomLayout> rooms;
     [SerializeField] private GameObject gridManager;
+    
+    
+    //awake method makes sure that LevelManager is not destroyed
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
     
     // Start is called before the first frame update
     void Start()
