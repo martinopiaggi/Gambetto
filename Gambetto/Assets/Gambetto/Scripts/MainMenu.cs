54e018fbadbe67fde152cb75b8d2d1d1024ef972
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-11-20 11:45:31 +0100

    added first Fog version, temporary level selection screen and new font

--- Gambetto/Assets/Gambetto/Scripts/MainMenu.cs
+++ Gambetto/Assets/Gambetto/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level selection");
     }
 
     public void QuitGame()
