63955c815401c14041e635e3eb2190babc9a7762
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-11-23 23:36:50 +0100

    Implemented greedy search AI correctly (multiple concurrent enemies)

--- Gambetto/Assets/Gambetto/Scripts/CPUBehavior.cs
+++ Gambetto/Assets/Gambetto/Scripts/CPUBehavior.cs
@@ -8,10 +8,10 @@ namespace Gambetto.Scripts
     public class CPUBehavior : MonoBehaviour
     {
         private GameObject _selectedSquare;
-        private Cell _chosenMoves;
+        private Dictionary<Piece,Cell> _chosenMoves;
         private Cell _playerCell;
 
-        public Cell ChosenMoves
+        public Dictionary<Piece,Cell> ChosenMoves
         {
             get => _chosenMoves;
             set => _chosenMoves = value;
@@ -20,44 +20,65 @@ namespace Gambetto.Scripts
 
         
         // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
             var selectedSquarePrefab = Resources.Load<GameObject>("Prefabs/Square");
             //In start I create the light used to illuminate the grid
             _selectedSquare = Instantiate(selectedSquarePrefab);
             _selectedSquare.SetActive(false);
+            _chosenMoves = new Dictionary<Piece, Cell>();
         }
         
         
-        public void StartComputing(Piece enemy, Cell enemyCell, Cell playerCell) //todo enemies!
+        public void StartComputing(Cell playerCell, Dictionary<Piece,Cell> enemies) //todo enemies!
         {
             _playerCell = playerCell;
-            //foreach (var enemyRef in enemies)
-            //{
-            StartCoroutine(ComputeNextMove(enemy, enemyCell));
-         //   }
+            foreach (var enemyRef in enemies)
+            {
+                StartCoroutine(ComputeNextMove(enemyRef.Key, enemyRef.Value));
+            }
         }
 
         private IEnumerator ComputeNextMove(Piece piece,Cell cell)
         {
             var clockPeriod = GameClock.Instance.ClockPeriod;
             var possibleMovements = PlayerController.GetPossibleMovements(piece, cell);
-            var choosenMove = cell;
+            var chosenMove = cell;
             var minDist = float.MaxValue;
             
             foreach (var move in possibleMovements)
             {
                 var dist = move.getGlobalCoordinates() - _playerCell.getGlobalCoordinates();
+                if(move.isEmpty())continue;
+                if (ThereIsSomeone(move)) continue;
                 if (dist.magnitude < minDist)
-                {
+                { 
                     minDist = dist.magnitude;
-                    choosenMove = move;
+                    chosenMove = move;
                 }
             }
-            Debug.Log("Old position: " + cell.getGlobalCoordinates() + "\nCPU has chosen: " + choosenMove.getGlobalCoordinates() + " as next move");
-            _chosenMoves = choosenMove; //todo now for testing
+
+            Debug.Log("Old position: " 
+                      + cell.getGlobalCoordinates() + "\nCPU has chosen: " 
+                      + chosenMove.getGlobalCoordinates() + " as next move");
+            _chosenMoves[piece] = chosenMove; //todo now for testing
             yield return null;
         }
+
         
+        public bool ThereIsSomeone(Cell here)
+        {
+            if(_playerCell == here) return true;
+            foreach(var enemy in _chosenMoves)
+            {
+                if (enemy.Value == here)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }  
+              
     }
 }
