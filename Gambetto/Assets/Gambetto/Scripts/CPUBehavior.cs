70fb70902bf20aaed0eed524cce24af4455e8d53
zibas <milo.brontesi@gmail.com>
2023-11-26 19:37:13 +0100

    Moves CPU behaviour calculation to a single coroutine

--- Gambetto/Assets/Gambetto/Scripts/CPUBehavior.cs
+++ Gambetto/Assets/Gambetto/Scripts/CPUBehavior.cs
@@ -8,17 +8,16 @@ namespace Gambetto.Scripts
     public class CPUBehavior : MonoBehaviour
     {
         private GameObject _selectedSquare;
-        private Dictionary<Piece,Cell> _chosenMoves;
+        private Dictionary<Piece, Cell> _chosenMoves;
         private Cell _playerCell;
+        private Coroutine _computingCoroutine;
 
-        public Dictionary<Piece,Cell> ChosenMoves
+        public Dictionary<Piece, Cell> ChosenMoves
         {
             get => _chosenMoves;
             set => _chosenMoves = value;
         }
 
-
-        
         // Start is called before the first frame update
         void Awake()
         {
@@ -28,48 +27,60 @@ namespace Gambetto.Scripts
             _selectedSquare.SetActive(false);
             _chosenMoves = new Dictionary<Piece, Cell>();
         }
-        
-        
-        public void StartComputing(Cell playerCell, Dictionary<Piece,Cell> enemies) //todo enemies!
+
+        public void StartComputing(Cell playerCell, Dictionary<Piece, Cell> enemies) //todo enemies!
+        {
+            if (_computingCoroutine != null)
+                StopCoroutine(_computingCoroutine);
+
+            _computingCoroutine = StartCoroutine(ComputingCoroutine(playerCell, enemies));
+        }
+
+        public IEnumerator ComputingCoroutine(Cell playerCell, Dictionary<Piece, Cell> enemies)
         {
             _playerCell = playerCell;
             foreach (var enemyRef in enemies)
             {
-                StartCoroutine(ComputeNextMove(enemyRef.Key, enemyRef.Value));
+                ComputeNextMove(enemyRef.Key, enemyRef.Value);
             }
+            yield return null;
         }
 
-        private IEnumerator ComputeNextMove(Piece piece,Cell cell)
+        private void ComputeNextMove(Piece piece, Cell cell)
         {
-            var clockPeriod = GameClock.Instance.ClockPeriod;
             var possibleMovements = PlayerController.GetPossibleMovements(piece, cell);
             var chosenMove = cell;
             var minDist = float.MaxValue;
-            
+
             foreach (var move in possibleMovements)
             {
                 var dist = move.getGlobalCoordinates() - _playerCell.getGlobalCoordinates();
-                if(move.isEmpty())continue;
-                if (ThereIsSomeone(move)) continue;
+                if (move.isEmpty())
+                    continue;
+                if (ThereIsSomeone(move))
+                    continue;
                 if (dist.magnitude < minDist)
-                { 
+                {
                     minDist = dist.magnitude;
                     chosenMove = move;
                 }
             }
 
-            Debug.Log("Old position: " 
-                      + cell.getGlobalCoordinates() + "\nCPU has chosen: " 
-                      + chosenMove.getGlobalCoordinates() + " as next move");
+            Debug.Log(
+                "Old position: "
+                    + cell.getGlobalCoordinates()
+                    + "\nCPU has chosen: "
+                    + chosenMove.getGlobalCoordinates()
+                    + " as next move"
+            );
             _chosenMoves[piece] = chosenMove; //todo now for testing
-            yield return null;
         }
 
-        
         public bool ThereIsSomeone(Cell here)
         {
-            if(_playerCell == here) return true;
-            foreach(var enemy in _chosenMoves)
+            if (_playerCell == here)
+                return true;
+            foreach (var enemy in _chosenMoves)
             {
                 if (enemy.Value == here)
                 {
@@ -78,7 +89,6 @@ namespace Gambetto.Scripts
             }
 
             return false;
-        }  
-              
+        }
     }
 }
