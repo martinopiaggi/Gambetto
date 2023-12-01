505870de8a991cbc80ae28ab9d008bd861a85e8a
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-12-01 17:17:59 +0100

    Add Scriptable Objects for Movement List

---
+++ Gambetto/Assets/Gambetto/Scripts/RoomsLayouts/Behaviour.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Gambetto.Scripts.Pieces;
+using Gambetto.Scripts.Utils;
+using Pieces;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "Behaviour", menuName = "ScriptableObjects/Behaviour")]
+public class Behaviour : ScriptableObject
+{
+
+    [SerializeField] private List<Vector2Int> _movements;
+    [SerializeField]
+    private int _offset;
+
+    public List<Vector2Int> Movements => _movements;
+
+    public int Offset => _offset;
+}
