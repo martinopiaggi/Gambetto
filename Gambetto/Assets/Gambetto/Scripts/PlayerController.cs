9d715cd67980aa73d1a37fcafed016f4a516e989
Mario2414 <mario.vallone@mail.polimi.it>
2023-12-01 17:10:47 +0100

    Partial initial refactor of CPUBehaviour

--- Gambetto/Assets/Gambetto/Scripts/PlayerController.cs
+++ Gambetto/Assets/Gambetto/Scripts/PlayerController.cs
@@ -96,67 +96,6 @@ namespace Gambetto.Scripts
             _selectedSquare.SetActive(false);
         }
 
-        //static just because now we are reusing it in the CPUBehavior
-        // todo: maybe considering to move to another class
-        public static List<Cell> GetPossibleMovements(Piece piece, Cell currentCell)
-        {
-            var possibleMovement = new List<Cell>();
-            Cell tempCell;
-            var directions = piece.PossibleMoves;
-
-            switch (piece.PieceType)
-            {
-                case PieceType.Bishop:
-                case PieceType.Queen:
-                case PieceType.Rook:
-                    foreach (var direction in directions)
-                    {
-                        tempCell = currentCell;
-                        while (tempCell?.getNext(direction) != null)
-                        {
-                            var nextCell = tempCell.getNext(direction);
-                            if (tempCell.isEmpty())
-                                break;
-                            tempCell = nextCell;
-                            possibleMovement.Add(tempCell);
-                        }
-                    }
-                    break;
-                case PieceType.Pawn:
-                case PieceType.King:
-                    foreach (var direction in directions)
-                    {
-                        tempCell = currentCell;
-                        tempCell = tempCell.getNext(direction);
-                        if (tempCell != null)
-                        {
-                            possibleMovement.Add(tempCell);
-                        }
-                    }
-                    break;
-                case PieceType.Knight:
-                    var i = 0;
-                    while (i < directions.Count)
-                    {
-                        tempCell = currentCell;
-                        for (var j = 0; j < 3; j++)
-                        {
-                            tempCell = tempCell.getNext(directions[i + j]);
-                            if (tempCell == null)
-                                break;
-                        }
-
-                        i = 3 + i;
-                        if (tempCell != null)
-                            possibleMovement.Add(tempCell);
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            return possibleMovement;
-        }
-
         public List<Cell> GetPossibleMovementsP(Piece piece, Cell currentCell)
         {
             var possibleMovement = new List<Cell>();
