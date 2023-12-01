f6339a88782c6bfbeaa09048c3580aa91ae899e4
Mario2414 <mario.vallone@mail.polimi.it>
2023-12-01 23:53:33 +0100

    Refactor of getPossibleMovement
    Now GetPossibleMovement is inside a util class, beccause was used by more classes

--- Gambetto/Assets/Gambetto/Scripts/PlayerController.cs
+++ Gambetto/Assets/Gambetto/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Gambetto.Scripts.Pieces;
+using Gambetto.Scripts.Utils;
 using UnityEngine;
 
 namespace Gambetto.Scripts
@@ -59,7 +60,7 @@ namespace Gambetto.Scripts
             MovePath.Add(_currentCell.getGlobalCoordinates());
             _possibleMovements.Clear();
             _possibleMovementsPath.Clear();
-            _possibleMovements = GetPossibleMovementsP(piece, currentCell);
+            _possibleMovements = PieceMovement.GetPossibleMovements(piece, currentCell, _possibleMovementsPath);
             _cycleMovesCoroutine = StartCoroutine(CycleMoves());
         }
 
@@ -95,83 +96,6 @@ namespace Gambetto.Scripts
             }
             _selectedSquare.SetActive(false);
         }
-
-        public List<Cell> GetPossibleMovementsP(Piece piece, Cell currentCell)
-        {
-            var possibleMovement = new List<Cell>();
-            var possibleMovementPath = new List<List<Vector3>>();
-            Cell tempCell;
-            List<Vector3> tempPath;
-            var directions = piece.PossibleMoves;
-
-            switch (piece.PieceType)
-            {
-                case PieceType.Bishop:
-                case PieceType.Queen:
-                case PieceType.Rook:
-                    foreach (var direction in directions)
-                    {
-                        tempCell = currentCell;
-                        while (tempCell?.getNext(direction) != null)
-                        {
-                            var nextCell = tempCell.getNext(direction);
-                            if (tempCell.isEmpty())
-                                break;
-                            tempCell = nextCell;
-                            tempPath = new List<Vector3>();
-                            tempPath.Add(tempCell.getGlobalCoordinates());
-                            possibleMovement.Add(tempCell);
-                            possibleMovementPath.Add(tempPath);
-                        }
-                    }
-                    break;
-                case PieceType.Pawn:
-                case PieceType.King:
-                    foreach (var direction in directions)
-                    {
-                        tempCell = currentCell;
-                        tempCell = tempCell.getNext(direction);
-                        if (tempCell != null)
-                        {
-                            tempPath = new List<Vector3>();
-                            possibleMovement.Add(tempCell);
-                            tempPath.Add(tempCell.getGlobalCoordinates());
-                            possibleMovementPath.Add(tempPath);
-                        }
-                    }
-                    break;
-                case PieceType.Knight:
-                    var i = 0;
-                    while (i < directions.Count)
-                    {
-                        tempCell = currentCell;
-                        tempPath = new List<Vector3>();
-                        for (var j = 0; j < 3; j++)
-                        {
-                            tempCell = tempCell.getNext(directions[i + j]);
-                            if (tempCell == null)
-                                break;
-                            if (j == 1)
-                            {
-                                tempPath.Add(tempCell.getGlobalCoordinates());
-                            }
-                        }
-
-                        i = 3 + i;
-                        if (tempCell != null)
-                        {
-                            possibleMovement.Add(tempCell);
-                            tempPath.Add(tempCell.getGlobalCoordinates());
-                            possibleMovementPath.Add(tempPath);
-                        }
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            _possibleMovementsPath = possibleMovementPath;
-            return possibleMovement;
-        }
+        
     }
 }
--- Gambetto/Assets/Gambetto/Scripts/Utils/PieceMovement.cs
+++ Gambetto/Assets/Gambetto/Scripts/Utils/PieceMovement.cs
@@ -1,7 +1,86 @@
-﻿namespace Gambetto.Scripts.Utils
+﻿using System;
+using System.Collections.Generic;
+using Gambetto.Scripts.Pieces;
+using UnityEngine;
+
+namespace Gambetto.Scripts.Utils
 {
-    public class PieceMovement
+    public static class PieceMovement
     {
-        
+        public static List<Cell> GetPossibleMovements(Piece piece, Cell currentCell,  List<List<Vector3>> possiblePaths)
+        {
+            var possibleMovement = new List<Cell>();
+            var possibleMovementPath = possiblePaths;
+            Cell tempCell;
+            List<Vector3> tempPath;
+            var directions = piece.PossibleMoves;
+
+            switch (piece.PieceType)
+            {
+                case PieceType.Bishop:
+                case PieceType.Queen:
+                case PieceType.Rook:
+                    foreach (var direction in directions)
+                    {
+                        tempCell = currentCell;
+                        while (tempCell?.getNext(direction) != null)
+                        {
+                            var nextCell = tempCell.getNext(direction);
+                            if (tempCell.isEmpty())
+                                break;
+                            tempCell = nextCell;
+                            tempPath = new List<Vector3>();
+                            tempPath.Add(tempCell.getGlobalCoordinates());
+                            possibleMovement.Add(tempCell);
+                            possibleMovementPath.Add(tempPath);
+                        }
+                    }
+                    break;
+                case PieceType.Pawn:
+                case PieceType.King:
+                    foreach (var direction in directions)
+                    {
+                        tempCell = currentCell;
+                        tempCell = tempCell.getNext(direction);
+                        if (tempCell != null)
+                        {
+                            tempPath = new List<Vector3>();
+                            possibleMovement.Add(tempCell);
+                            tempPath.Add(tempCell.getGlobalCoordinates());
+                            possibleMovementPath.Add(tempPath);
+                        }
+                    }
+                    break;
+                case PieceType.Knight:
+                    var i = 0;
+                    while (i < directions.Count)
+                    {
+                        tempCell = currentCell;
+                        tempPath = new List<Vector3>();
+                        for (var j = 0; j < 3; j++)
+                        {
+                            tempCell = tempCell.getNext(directions[i + j]);
+                            if (tempCell == null)
+                                break;
+                            if (j == 1)
+                            {
+                                tempPath.Add(tempCell.getGlobalCoordinates());
+                            }
+                        }
+
+                        i = 3 + i;
+                        if (tempCell != null)
+                        {
+                            possibleMovement.Add(tempCell);
+                            tempPath.Add(tempCell.getGlobalCoordinates());
+                            possibleMovementPath.Add(tempPath);
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return possibleMovement;
+        }
     }
 }
