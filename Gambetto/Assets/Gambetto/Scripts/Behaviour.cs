841586cd576759c29d1706ae693117a7d341f087
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-12-06 16:52:57 +0100

    added aggressive condition for BFS behavior

--- Gambetto/Assets/Gambetto/Scripts/Behaviour.cs
+++ Gambetto/Assets/Gambetto/Scripts/Behaviour.cs
@@ -13,11 +13,13 @@ public class Behaviour : ScriptableObject
 {
 
     [SerializeField] private List<Vector2Int> _movements;
-    [SerializeField]
-    private int _offset;
-    [SerializeField]
-    private int id;
+    [SerializeField] private int _offset;
+    [SerializeField] private int id;
+    [SerializeField] private bool aggressive;
+    
+    
     public int Id => id;
+    public bool Aggressive => aggressive;
 
     public List<Vector2Int> Movements => _movements;
 
