f6542f5325cc8b0469f931586f3b4686fb362b7d
zibas <milo.brontesi@gmail.com>
2023-12-29 18:09:24 +0100

    Reworks remaining ingame menus + some code cleanup

---
+++ Gambetto/Assets/Gambetto/Scripts/UI/GeneralMenuFunctions.cs
@@ -0,0 +1,39 @@
+using Gambetto.Scripts.GameCore.Grid;
+using Gambetto.Scripts.Utils;
+using UnityEngine;
+
+namespace Gambetto.Scripts.UI
+{
+    public class GeneralMenuFunctions: MonoBehaviour
+    {
+        private GridManager gridManager;
+
+        private void Awake()
+        {
+            gridManager = FindObjectOfType<GridManager>();
+        }
+
+        /// <summary>
+        /// Restart the level and sets a given menu to inactive,
+        /// </summary>
+        /// <param name="menuToClose">Menu to set inactive.</param>
+        public void RestartAndCloseMenu(GameObject menuToClose = null)
+        {
+            if (menuToClose != null) menuToClose.SetActive(false);
+            TimeManager.ResumeTime();
+            gridManager.RestartLevel();
+        }
+
+        public void BackToLevels(GameObject menuToClose =null)
+        {
+            if (menuToClose != null) menuToClose.SetActive(false);
+            GameManager.Instance.sceneTransition.CrossFade("Level selection");
+            TimeManager.ResumeTime();
+        }
+        
+        public void ResumeGame()
+        {
+            TimeManager.ResumeTime();
+        }
+    }
+}
