e66b61bee0fafd0bdd78adcd09b24f9f71a4c754
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-12-30 17:38:07 +0100

    Clock periods moved into class PieceConstants

--- Gambetto/Assets/Gambetto/Scripts/GameCore/Piece/PieceConstants.cs
+++ Gambetto/Assets/Gambetto/Scripts/GameCore/Piece/PieceConstants.cs
@@ -1,7 +1,23 @@
-﻿namespace Gambetto.Scripts.GameCore.Piece
+﻿using System.Collections.Generic;
+
+namespace Gambetto.Scripts.GameCore.Piece
 {
     public static class PieceConstants
     {
+        /// <summary>
+        /// Dictionary of clock period values for each piece.
+        /// </summary>
+        public static readonly Dictionary<PieceType, float> TypesClockPeriods =
+            new()
+            {
+                { PieceType.Pawn, 2.5f },
+                { PieceType.Bishop, 3.7f },
+                { PieceType.Knight, 3f },
+                { PieceType.Rook, 3f },
+                { PieceType.King, 2.5f }, //king not implemented
+                { PieceType.Queen, 2.5f } //queen not implemented
+            };
+
         public const int MaxPieceCountdown = 20;
         public const int MinPieceCountdown = 1;
 
