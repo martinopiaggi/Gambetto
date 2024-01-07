d1837405cc8a06848a03b718759dd65d218b6133
zibas <milo.brontesi@gmail.com>
2024-01-07 19:35:42 +0100

    Fixes transition color + adds color scheme to level selector

--- Gambetto/Assets/Gambetto/Scripts/ColorScheme.cs
+++ Gambetto/Assets/Gambetto/Scripts/ColorScheme.cs
@@ -4,41 +4,44 @@ namespace Gambetto.Scripts
 {
     public class ColorScheme : MonoBehaviour
     {
-    
         [SerializeField]
         public Material transitionMaterial;
-        
+
+        [SerializeField]
+        private Color trasitionColor;
+
         [SerializeField]
         public Material fogMaterial;
-        
+
         [SerializeField]
         private Color fogColor;
-    
+
         [SerializeField]
         public Material endLevelMaterial;
-        
+
         [SerializeField]
         private Color endLevelColor;
-        
+
         [SerializeField]
         public Material lateralMaterial;
-        
+
         [SerializeField]
         private Color lateralColor;
-    
+
         [SerializeField]
         public Material lightMaterial;
-        
+
         [SerializeField]
         private Color lightColor;
-        
+
         [SerializeField]
         public Material darkMaterial;
-        
+
         [SerializeField]
         private Color darkColor;
 
         public bool liveEdit;
+
         private void Start()
         {
             UpdateMaterialColors(); // Initialize material colors
@@ -54,7 +57,7 @@ namespace Gambetto.Scripts
 
         private void UpdateMaterialColors()
         {
-            transitionMaterial.color = fogColor;
+            transitionMaterial.color = trasitionColor;
             fogMaterial.SetColor("_FogColor", fogColor);
             endLevelMaterial.color = endLevelColor;
             lateralMaterial.color = lateralColor;
